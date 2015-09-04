282ced99d2df3a66fecaf6e5501c314855a1e26d
cburkins <chad.burkins@gmail.com>
2015-09-04 19:25:13 +0000

    Added commets to csvload.sh, heavily modified merge_files.sh

--- csvload.sh
+++ csvload.sh
@@ -15,6 +15,16 @@ if [ ! -f $input_file ]; then
    echo; exit
 fi
 
+# Look for valid data lines within the file
+# Valid lines look like this:
+#    3204088,itsbus40,0,Backup,itshru01.backup.jnj.com,"May 15, 2015 9:00:16 AM","May 15, 2015 9:00:50 AM",00:00:34,"208,256",1,"22,653"
+# Valid data lines will have a comma followed by a duration within the line
+# e.g.    ,00:00:34
+# e.g.    ,4:23:00
+# Example of non-valid line: Comments at the top and bottom of file
+# Example of non-valid line: Occasionally there's a weird line like this:
+#    6582438,ncsebus,150,Backup,UNKNOWN,-,"May 20, 2015 12:14:55 PM",13651431295,0,0,0
+# NOTE: this technique is also used in the merge_files.sh script
 cat $input_file | egrep  '\,[0-9][0-9]*:[0-9][0-9]*:[0-9][0-9]*\,' > $tmp_file
 
 count_orig=`wc -l $input_file | awk '{print $1}'`
@@ -32,6 +42,7 @@ read
 echo "Go..."
 echo 
 
+# Load the data lines into logstash, using my csvload configuration file
 cat $tmp_file | /opt/logstash/bin/logstash -f ./csvload.conf
 
 /bin/rm $tmp_file
--- merge_files.sh
+++ merge_files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo
+
 tmp_file=/tmp/merged.$$
+file1_valid=/tmp/file1.$$
+file2_valid=/tmp/file2.$$
+
 
 if [ $# -ne 3 ]; then
    echo
@@ -23,37 +28,64 @@ if [ ! -f $file2 ]; then
    echo; exit
 fi
 
+printf "Parsing for valid lines..."
+# Look for valid data lines within the file
+# Valid lines look like this:
+#    3204088,itsbus40,0,Backup,itshru01.backup.jnj.com,"May 15, 2015 9:00:16 AM","May 15, 2015 9:00:50 AM",00:00:34,"208,256",1,"22,653"
+# Valid data lines will have a comma followed by a duration within the line
+# e.g.    ,00:00:34
+# e.g.    ,4:23:00
+# Example of non-valid line: Comments at the top and bottom of file
+# Example of non-valid line: Occasionally there's a weird line like this:
+#    6582438,ncsebus,150,Backup,UNKNOWN,-,"May 20, 2015 12:14:55 PM",13651431295,0,0,0
+# NOTE: this technique is also used in the csvload.sh script
+cat $file1 | egrep  '\,[0-9][0-9]*:[0-9][0-9]*:[0-9][0-9]*\,' > $file1_valid
+cat $file2 | egrep  '\,[0-9][0-9]*:[0-9][0-9]*:[0-9][0-9]*\,' > $file2_valid
+echo "done"
 
 file1_line_count=`wc -l $file1 | awk '{print $1}'`
-file2_line_count=`wc -l $file2 | awk '{print $1}'`
-overlap_count=`cat $file1 $file2 | sort | uniq -d | wc -l`
-merged_count=`cat $file1 $file2 | sort | uniq -u | wc -l`
-total_count=`cat $file1 $file2 | wc -l`
+file1_valid_count=`wc -l $file1_valid | awk '{print $1}'`
+diff1=`echo "$file1_line_count - $file1_valid_count" | bc`
 
-#delta=`echo "$count_orig - $count_good" | bc`
+
+file2_line_count=`wc -l $file2 | awk '{print $1}'`
+file2_valid_count=`wc -l $file2_valid | awk '{print $1}'`
+diff2=`echo "$file2_line_count - $file2_valid_count" | bc`
+
+printf "Calculating unique counts..."
+overlap_count=`cat $file1_valid $file2_valid | sort | uniq -d | wc -l`
+merged_count=`cat $file1_valid $file2_valid | sort | uniq -u | wc -l`
+total_count=`cat $file1_valid $file2_valid | wc -l`
+overlap_pct=`echo "scale=4; $overlap_count / $total_count * 100" | bc -l`
+merged_pct=`echo "scale=4; $merged_count / $total_count * 100" | bc -l`
+echo "done"
+
 
 echo
 echo "            file1 = $file1"
 echo "            file2 = $file2"
 echo "      merged file = $mergedfile"
 echo 
-printf " file1 line count = %'10d\n" $file1_line_count
-printf " file2 line count = %'10d\n" $file2_line_count
-printf "      total count = %'10d\n" $total_count
-printf "     merged count = %'10d\n" $merged_count
-printf "    overlap count = %'10d\n" $overlap_count
+printf " file1 line  count = %'10d\n" $file1_line_count
+printf " file1 valid count = %'10d (-%d)\n" $file1_valid_count $diff1
+printf " file2 line  count = %'10d\n" $file2_line_count
+printf " file2 valid count = %'10d (-%d)\n" $file2_valid_count $diff2
+printf "       total count = %'10d\n" $total_count
+printf "      merged count = %'10d (%05.2f%%)\n" $merged_count $merged_pct
+printf "     overlap count = %'10d (%05.2f%%)\n" $overlap_count $overlap_pct
 echo
 
 echo "Hit <return> to continue..."
 read
-echo "Go..."
-echo 
+printf "Merging two input files and creating output file..."
+echo "done"
+echo
 
-cat $file1 $file2 | sort | uniq -u > $mergedfile
+cat $file1_valid $file2_valid | sort | uniq -u > $mergedfile
 
 merged_count=`wc -l $mergedfile | awk '{print $1}'`
 printf "    merged count = %'.f\n" $merged_count
 echo
 
-/bin/rm -f $tmp_file
+/bin/rm -f $tmp_file $file1_valid $file2_valid
 
